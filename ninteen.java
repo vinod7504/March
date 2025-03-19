2c9aebd75561ef0fd3eb9d49a5814e3138678158
vinod7504 <vinodkumarjntua@gmail.com>
2025-03-19 07:55:44 +0530

    Added ninteen

---
+++ ninteen.java
@@ -0,0 +1,22 @@
+public class ninteen{
+    public static void main(String[] args) {
+        int[] nums={0,1,1,0,1,1};
+        System.out.println(minOperation(nums));
+    }
+    public static int minOperations(int[] nums) {
+        int res=0;
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==0){
+                nums[i]=1;
+                nums[i+1]=nums[i+1]^1;
+                nums[i+2]=nums[i+2]^1;
+                res++;
+            }
+        }
+        if(nums[n-1]==0 || nums[n-2]==0){
+            return -1;
+        }
+        return res;
+    }
+}
